3701d10a391d69b01b30791133788c9e2d9de358
Jan Vanbuel <janvanbuel@hotmail.com>
2023-10-12 14:50:52 +0200

    use object_store put instead of blocking polars writer

--- examples/azure/src/parquet/main.rs
+++ examples/azure/src/parquet/main.rs
@@ -1,12 +1,16 @@
 use log::info;
 use object_store::azure::MicrosoftAzureBuilder;
 use object_store::path::Path;
+use object_store::ObjectStore;
 use polars::io::parquet::ParquetWriter;
-use polars::{io::cloud::CloudWriter, prelude::*};
+use polars::prelude::*;
 use simplelog::SimpleLogger;
 use simplelog::{Config, LevelFilter};
 
-fn main() -> Result<(), PolarsError> {
+const OUTPUT_LOCATION: &str = "output.parquet";
+
+#[tokio::main]
+async fn main() -> Result<(), PolarsError> {
     let _ = SimpleLogger::init(LevelFilter::Info, Config::default());
 
     let mut s = DataFrame::new(vec![
@@ -21,11 +25,13 @@ fn main() -> Result<(), PolarsError> {
         .with_use_azure_cli(true)
         .build()?;
 
-    let cloud_writer =
-        CloudWriter::new_with_object_store(Arc::from(object_store), Path::from("example.parquet"))?;
-
     info!("Writing to cloud storage");
-    ParquetWriter::new(cloud_writer).finish(&mut s)?;
+    let mut buffer = vec![];
+
+    ParquetWriter::new(&mut buffer).finish(&mut s)?;
+    object_store
+        .put(&Path::from(OUTPUT_LOCATION), buffer.into())
+        .await?;
 
     info!("Done!");
     Ok(())
