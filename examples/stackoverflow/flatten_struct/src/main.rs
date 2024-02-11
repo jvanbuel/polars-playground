75e3bc0fbfca493a7f2118b32eec0485830647d9
Jan Vanbuel <janvanbuel@hotmail.com>
2024-02-11 14:35:14 +0100

    flatten_struct

---
+++ examples/stackoverflow/flatten_struct/src/main.rs
@@ -0,0 +1,50 @@
+use polars::prelude::*;
+
+struct Record {
+    field1: String,
+    field2: i32,
+}
+
+macro_rules! struct_to_dataframe {
+    ($input:expr, [$($field:ident),+]) => {
+        {
+            // Extract the field values into separate vectors
+            $(let mut $field = Vec::new();)*
+
+            for e in $input.into_iter() {
+                $($field.push(e.$field);)*
+            }
+            df!(
+                $(stringify!($field) => $field,)*
+            )
+        }
+    };
+}
+
+fn main() -> PolarsResult<()> {
+    let df: DataFrame = struct_to_dataframe!(
+        vec![
+            Record {
+                field1: "val1".into(),
+                field2: 1
+            },
+            Record {
+                field1: "val2".into(),
+                field2: 2
+            }
+        ],
+        [field1, field2]
+    )?;
+    println!("{:?}", df);
+    // shape: (2, 2)
+    // ┌────────┬────────┐
+    // │ field1 ┆ field2 │
+    // │ ---    ┆ ---    │
+    // │ str    ┆ i32    │
+    // ╞════════╪════════╡
+    // │ val1   ┆ 1      │
+    // │ val2   ┆ 2      │
+    // └────────┴────────┘
+
+    Ok(())
+}
